3e50929e1b7cd54eff0da2043b3ab9e43cf2c6bc
EstebanDev <guachichullcaesteban@gmail.com>
2025-07-07 17:37:41 -0500

    feat: Implement authentication with Google and update layout for session management
    - Added authentication configuration using Google provider.
    - Updated Layout.astro to include session management and dynamic login/logout buttons.
    - Modified index.astro to display user information upon successful login.
    - Introduced global CSS file for styling with Tailwind CSS.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import auth from 'auth-astro';
+
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [auth()],
+  adapter: netlify(),
+  output: 'server',
+});
