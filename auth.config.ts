92e1919ae6dfb3903dae14b82ccbb774bf892213
EstebanDev <guachichullcaesteban@gmail.com>
2025-07-08 08:02:22 -0500

    Agregar configuración de autenticación para GitHub en auth.config.ts

--- auth.config.ts
+++ auth.config.ts
@@ -1,3 +1,4 @@
+import GitHub from '@auth/core/providers/github';
 import Google from '@auth/core/providers/google';
 import { defineConfig } from 'auth-astro';
 
@@ -7,5 +8,9 @@ export default defineConfig({
       clientId: import.meta.env.GOOGLE_CLIENT_ID,
       clientSecret: import.meta.env.GOOGLE_CLIENT_SECRET,
     }),
+    GitHub({
+      clientId: import.meta.env.GITHUB_CLIENT_ID,
+      clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
+    })
   ],
 });
