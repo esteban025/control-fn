8f1b43fe4656757ee3ab7c77ab7b099677a87aeb
EstebanDev <guachichullcaesteban@gmail.com>
2025-07-16 19:42:11 -0500

    Agregar interfaces Category y Subcategory, y actualizar la interfaz Expense para eliminar subcategory_id

---
+++ src/types/category.ts
@@ -0,0 +1,6 @@
+export interface Category {
+  id: string;
+  name: string;
+  description: string;
+  spent_limit: number;
+}
--- src/types/expense.ts
+++ src/types/expense.ts
@@ -1,19 +1,8 @@
-// CREATE TABLE expense (
-// 	id CHAR(36) PRIMARY KEY DEFAULT(uuid()),
-//     amount  DECIMAL(10,2) NOT NULL DEFAULT 0, 
-//     description TEXT,
-//     date DATE NOT NULL,
-//     category_id VARCHAR(50) NOT NULL,
-//     subcategory_id INT NOT NULL,
-    
-//     FOREIGN KEY (category_id) REFERENCES category(id),
-//     FOREIGN KEY (subcategory_id) REFERENCES subcategory(id)
-// );
 export interface Expense {
   id: string;
   amount: number;
   description: string;
   date: Date;
   category_id: string;
-  subcategory_id: number;
+  // subcategory_id: number;
 }
